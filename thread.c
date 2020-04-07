b7d0db3dce55327ce7a0d3277f6640547ae26ae5
raghavsharma804 <63308035+raghavsharma804@users.noreply.github.com>
2020-04-07 22:43:02 +0530

    Add files via upload

--- thread.c
+++ thread.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<pthread.h>
-int range;
-void *getrange(void *value)
+#include<pthread.h>    //contains all the libraries required of threads in C
+int range;             //global variable range declared
+void *getrange(void *value)    //start of the first thread
 {
 
-	printf("Welocme the first thread.\n\nEnter the number you wish to get prime numbers for: ");
-  scanf("%d",&range);
+  printf("\n\t\t\t\tWelocme the first thread.\n\n\t\t\t\tPlease Enter the number you wish to get prime numbers for: Range=");   //asks the user for entering the range
+  scanf("%d",&range);    //scans the range integer and saves
+  printf("\n\n\t\t\t\tYour request is saved. This is the end of Thread-1");  //end of thread-1 message
 	return NULL;
-}
-void *prime(void *disp)
+} //end of thread-1
+void *prime(void *disp)  //start of thread 2
 {
-  sleep(10);
-  int i;
- printf("\nThis is start of second thread. \n\nThe number you entered is :%d",range);
- printf("\nPrime numbers are:-\n");    
-    for(i=2;i<=range;i++)
+  int i;  //for interating of loop
+ printf("\n\n\n\t\t\t\tWelcome to the second thread.");
+ printf("\n\n\t\t\t\tPrime numbers that exist between 0 to %d are as follows:-\n",range);    
+    for(i=2;i<=range;i++)   //start of for loop for checking prime numbers.
     {
         int c=0;
         for(int j=1;j<=i;j++)
@@ -27,7 +27,7 @@ void *prime(void *disp)
         } 
        if(c==2)
         {
-            printf("%d\n",i);
+            printf("\t\t%d\n",i);
         }
     }
  return NULL;
@@ -35,9 +35,10 @@ void *prime(void *disp)
 
 int main()
 {
-  pthread_t threadone;
-  pthread_t threadtwo;
-  pthread_create(&threadone,NULL,getrange,NULL);
+  pthread_t threadone; //declaration of Thread-1
+  pthread_t threadtwo;  //declaration of Thread-2
+  pthread_create(&threadone,NULL,getrange,NULL); //creation of Thread-1:
+  pthread_join(threadone,NULL);
   pthread_create(&threadtwo,NULL,prime,NULL);
   pthread_exit(NULL);
 }
